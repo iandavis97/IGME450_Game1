3d637e3e7102dd1c13e3187ebc2b88f0cbafd0a3
Jonathan So <jds7523@rit.edu>
2019-01-19 19:38:53 -0500

    Changed the way facing left/right works and added oscillating jump controls.
    There is also commented-out code that provides an alternative to the
    oscillating jump where upon reaching max strength, it'll go directly
    back to minimum strength, starting the charge all over again.

--- Assets/Cactus/Cactus.cs
+++ Assets/Cactus/Cactus.cs
@@ -8,7 +8,7 @@ using System.Collections;
 public class Cactus : MonoBehaviour {
 
 	public KeyCode cactus; // The key to hit for jumping.
-    public bool facingRight = true;//used for changing directions, should always start facing right
+    public int facingRight = 1;//used for changing directions, should always start facing right. +1 is right, -1 is left. Possible values are +1 and -1.
 
 	[SerializeField]
 	private float jumpFX = 0f; // Horizontal Force to apply to make the cactus jump.
@@ -42,37 +42,59 @@ public class Cactus : MonoBehaviour {
 		if (Input.GetKeyUp(cactus) && !anim.GetBool("InAir")) { // Release our jump.
 			StopCoroutine(currCoroutine);
 			sr.color = Color.white;
-			rb.AddForce(new Vector2(jumpFX, jumpFY));
+			rb.AddForce(new Vector2(facingRight * jumpFX, jumpFY));
 			anim.SetBool("Charging", false);
 			anim.SetBool("InAir", true);
 		}
 
         //changing directions before jump
-        if(Input.GetKeyDown(KeyCode.LeftArrow)&&(facingRight==true))
-        {
-            facingRight = false;
-            transform.localRotation = Quaternion.Euler(0, 180, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow)&&facingRight==false)
-        {
-            facingRight = true;
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
+		if ((Input.GetKeyDown(KeyCode.LeftArrow)&&(facingRight == 1))||(Input.GetKeyDown(KeyCode.RightArrow)&&facingRight == -1)) {
+			Facing();
         }
     }
 		
 	// Charges the power of our jump. Jump strength is indicated by our color.
 	private IEnumerator ChargeJump() {
-		float timer = 0f;
+		float timer;
 		// Default "tap-jump" is a tiny hop that gets us off the ground. 
 		jumpFX = 100f;
 		jumpFY = 200f;
 		Color tempColor = Color.white;
+		// Oscillating jump
+		while (true) {
+			// Our jump strength increases...
+			timer = 0f;
+			while (jumpFX < MAX_JUMP_FX || jumpFY < MAX_JUMP_FY) { 
+				jumpFX = Mathf.Lerp(100, MAX_JUMP_FX, (timer / HOLD_TIME));
+				jumpFY = Mathf.Lerp(200, MAX_JUMP_FY, (timer / HOLD_TIME));
+				tempColor.r = Mathf.Lerp(Color.white.r, Color.red.r, (timer / HOLD_TIME));
+				tempColor.g = Mathf.Lerp(Color.white.g, Color.red.g, (timer / HOLD_TIME));
+				tempColor.b = Mathf.Lerp(Color.white.b, Color.red.b, (timer / HOLD_TIME));
+				sr.color = tempColor;
+				timer += Time.deltaTime;
+				yield return new WaitForSeconds(Time.deltaTime);
+			}
+			yield return new WaitForSeconds(Time.deltaTime);
+			// Then, it decreases.
+			timer = 0f;
+			while (jumpFX > 100 || jumpFY > 200) { 
+				jumpFX = Mathf.Lerp(MAX_JUMP_FX, 100, (timer / HOLD_TIME));
+				jumpFY = Mathf.Lerp(MAX_JUMP_FY, 200, (timer / HOLD_TIME));
+				tempColor.r = Mathf.Lerp(Color.red.r, Color.white.r, (timer / HOLD_TIME));
+				tempColor.g = Mathf.Lerp(Color.red.g, Color.white.g, (timer / HOLD_TIME));
+				tempColor.b = Mathf.Lerp(Color.red.b, Color.white.b, (timer / HOLD_TIME));
+				sr.color = tempColor;
+				timer += Time.deltaTime;
+				yield return new WaitForSeconds(Time.deltaTime);
+			}
+			yield return new WaitForSeconds(Time.deltaTime);
+		}
+
+		/* 
+		// The following code is for an implementation where, upon reaching the maximum strength, 
+		// the charge resets directly to the weakest strength, starting over from the beginning. We can choose whether we want this or an oscillating jump.
 		while (jumpFX < MAX_JUMP_FX || jumpFY < MAX_JUMP_FY) {
-            //changing direction depending on where player is faacing
-            if(facingRight==true)
-			    jumpFX = Mathf.Lerp(100, MAX_JUMP_FX, (timer / HOLD_TIME));
-            if(facingRight==false)
-                jumpFX = Mathf.Lerp(-100, MAX_JUMP_FX, (timer / HOLD_TIME));//NOTE: not tested yet
+			jumpFX = Mathf.Lerp(100, MAX_JUMP_FX, (timer / HOLD_TIME));
             jumpFY = Mathf.Lerp(200, MAX_JUMP_FY, (timer / HOLD_TIME));
 			tempColor.r = Mathf.Lerp(Color.white.r, Color.red.r, (timer / HOLD_TIME));
 			tempColor.g = Mathf.Lerp(Color.white.g, Color.red.g, (timer / HOLD_TIME));
@@ -81,7 +103,23 @@ public class Cactus : MonoBehaviour {
 			timer += Time.deltaTime;
 			yield return new WaitForSeconds(Time.deltaTime);
 		}
+		yield return new WaitForSeconds(1/4f);
+		StopCoroutine(currCoroutine);
+		currCoroutine = StartCoroutine(ChargeJump());
+		*/
 	}
+
+	/* Changes the direction that this object is facing.
+	 * We take our current scale and save a temporary copy; we then modify individual components
+	 * of it and then save it back into our current scale.
+	 */
+	private void Facing() {
+		Vector3 tempScale = transform.localScale;
+		tempScale.x *= -1;
+		facingRight *= -1;
+		transform.localScale = tempScale;
+	}
+
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.tag == "Ground") {
