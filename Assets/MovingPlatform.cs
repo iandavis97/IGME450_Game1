332426eeb7008e39893a0e7bb60456e7a6667432
Malcolm Lambrecht <jml1769@rit.edu>
2019-02-06 22:43:00 -0500

    Changed moving platforms around a bunch locally, this is the final result

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Malcolm Lambrecht
+ *  to make a moving platform attach this script to it and make sure to set at least two points to lerp between
+ *  also give the platform's box collider the "Platform Mat" material
+ */
+
+
+
 public class MovingPlatform : MonoBehaviour {
 
     public Vector2[] pointList;
     public float speed = 3.0f;
     public bool drawDebug;
-
 
     //get the parent object for lerping
     GameObject platform;
@@ -22,14 +28,12 @@ public class MovingPlatform : MonoBehaviour {
         platform = gameObject;
         currentPoint = 0;
         destinationPoint = currentPoint + 1;
+
         startTime = Time.time;
         distance = Vector3.Distance(pointList[currentPoint], pointList[currentPoint + 1]);
 
-        platform.transform.position = pointList[currentPoint];
+        gameObject.transform.position = pointList[currentPoint];
         numPoints = pointList.Length;
-
-        if (!gameObject.GetComponent<Rigidbody>())
-            Debug.Log("true");
 	}
 
 
@@ -40,7 +44,7 @@ public class MovingPlatform : MonoBehaviour {
         float distCovered = (Time.time - startTime) * speed;
         float pctCompleted = distCovered / distance;
 
-        platform.transform.position = Vector3.Lerp(pointList[currentPoint], pointList[destinationPoint], pctCompleted);
+        gameObject.transform.position = Vector3.Lerp(pointList[currentPoint], pointList[destinationPoint], pctCompleted);
 
         if(pctCompleted >= 1.0f)
         {
@@ -63,6 +67,7 @@ public class MovingPlatform : MonoBehaviour {
         }
     }
 
+    //for visual points that the platform will lerp between, editor only
     private void OnDrawGizmos()
     {
         if (drawDebug)
@@ -70,11 +75,12 @@ public class MovingPlatform : MonoBehaviour {
             Gizmos.color = Color.red;
             foreach (Vector2 point in pointList)
             {
-                Gizmos.DrawSphere(new Vector3(point.x, point.y, 0), 0.3f);
+                Gizmos.DrawSphere(new Vector3(point.x, point.y, 0), 0.2f);
             }
         }
     }
 
+    //parents the player to the platform, this is responsible for most of the not sliding off of them
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.transform.tag == "Player")
@@ -83,6 +89,7 @@ public class MovingPlatform : MonoBehaviour {
         }
     }
 
+    //unparents the player to the platform
     private void OnCollisionExit2D(Collision2D other)
     {
         if (other.transform.tag == "Player")
